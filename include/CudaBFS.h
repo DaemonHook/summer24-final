96281d771ef7378059cff5f480cca4b4da5bc0b1
wty <116503270+DaemonHook@users.noreply.github.com>
2024-08-28 16:20:57 +0800

    生成数据的脚本

--- include/CudaBFS.h
+++ include/CudaBFS.h
@@ -3,4 +3,8 @@
 #include "Graph.h"
 #include <cuda_runtime.h>
 
+/// @brief 
+/// @param graph 
+/// @param sourceId 
+/// @return 到其他点的距离（不考虑权重） 
 std::vector<int> cudaBFS(LinkGraph& graph, nodeId_t sourceId);
